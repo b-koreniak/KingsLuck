88b9fd3b106d042863f077dd5070603c8a3d7b13
Uuzvername <imrahill120@gmail.com>
2020-06-30 15:23:58 +0300

    [refac] Issue: #20
    Room is not sending messages from itself now. This logic was delegated to Server.
    Added proper connection lost handling on server side.
    
    Issues: https://github.com/b-koreniak/KingsLuck/issues/20

--- Server/src/main/java/koreniak/kingsluck/server/WebSocketServerEndpoint.java
+++ Server/src/main/java/koreniak/kingsluck/server/WebSocketServerEndpoint.java
@@ -5,17 +5,22 @@ import koreniak.kingsluck.core.message.MessageDecoder;
 import koreniak.kingsluck.core.message.MessageEncoder;
 import koreniak.kingsluck.core.net.SessionI;
 import koreniak.kingsluck.core.net.WebSocketSessionImpl;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @ServerEndpoint(value = "/server", encoders = {MessageEncoder.class}, decoders = {MessageDecoder.class})
 public class WebSocketServerEndpoint {
+    private static final Logger LOGGER = LogManager.getLogger("WebSocketServerEndpoint");
+
     private static final Map<String, SessionI> sessionMap = new ConcurrentHashMap<>();
 
-    private Server server = new Server();
+    private static final Server server = new Server();
 
     @OnOpen
     public void onOpen(Session session) {
@@ -24,18 +29,34 @@ public class WebSocketServerEndpoint {
 
     @OnMessage
     public void onMessage(Message message, Session session) {
-        server.handleMessage(sessionMap.get(session.getId()), message);
+        try {
+            server.handleMessage(sessionMap.get(session.getId()), message);
+        } catch (IOException | EncodeException e) {
+            LOGGER.error("onMessage handling exception", e);
+        }
     }
 
     @OnClose
     public void onClose(CloseReason closeReason, Session session) {
-        server.handleSessionClose(sessionMap.get(session.getId()));
+        try {
+            server.handleSessionClose(sessionMap.get(session.getId()));
+        } catch (IOException | EncodeException e) {
+            LOGGER.error("onClose handling exception", e);
+        }
+
         sessionMap.remove(session.getId());
     }
 
     @OnError
     public void onError(Session session, Throwable throwable) {
-        server.handleSessionClose(sessionMap.get(session.getId()));
+        try {
+            server.handleSessionClose(sessionMap.get(session.getId()));
+        } catch (IOException | EncodeException e) {
+            LOGGER.error("onError handling exception", e);
+        }
+
+        LOGGER.error("onError exception", throwable);
+
         sessionMap.remove(session.getId());
     }
 }
